5f9080f0b63bf0fb32114de51deaf6b6bec64e96
art-sov <artsovlov@gmail.com>
2018-09-10 11:09:34 +0300

    first commit

---
+++ app/src/main/java/com/art/beatbox/BeatBoxActivity.java
@@ -0,0 +1,16 @@
+package com.art.beatbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BeatBoxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beat_box);
+
+//        implementation 'com.android.support:recyclerview-v7:27.1.1'
+
+    }
+}
