485cbe397d9c4af8059217e1415604a99dc2bba8
art-sov <artsovlov@gmail.com>
2018-09-11 10:35:15 +0300

    chapter20 complite

--- app/src/main/java/com/art/beatbox/BeatBoxActivity.java
+++ app/src/main/java/com/art/beatbox/BeatBoxActivity.java
@@ -1,16 +1,11 @@
 package com.art.beatbox;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v4.app.Fragment;
 
-public class BeatBoxActivity extends AppCompatActivity {
+public class BeatBoxActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_beat_box);
-
-//        implementation 'com.android.support:recyclerview-v7:27.1.1'
-
+    protected Fragment createFragment() {
+        return BeatBoxFragment.newInstance();
     }
 }
